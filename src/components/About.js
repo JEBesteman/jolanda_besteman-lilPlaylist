fdf80383ab8fc9f3932e669c2899ad12306d67be
Jolanda Besteman <jebesteman@gmail.com>
2021-11-25 10:42:35 +0100

    routing works

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>Muziek is mijn alles!!</h2>
+      <p>
+        Dankzij muziek ben ik door de lastigste periodes van mijn leven gekomen.
+      </p>
+      <p>
+        Ik heb zelfs mijn hond Chester vernoemd naar mijn favoriete zanger van
+        Linkin Park, die helaas zelf overleden is.{" "}
+      </p>
+      <p>
+        Ik heb deze opdracht gemaakt met React en React router v6, voor nu
+        zonder Redux (toekomst!)
+      </p>
+    </div>
+  );
+};
+
+export default About;
